e2d05447cb7d7477d0912558b9c2e5e4ff9dd3b9
jueun0801 <109045625+jueun0801@users.noreply.github.com>
2023-12-04 13:45:13 -0800

    Add parse function declaration and definition in class Roster

--- roster.cpp
+++ roster.cpp
@@ -1,4 +1,58 @@
 #include <iostream>
 #include <string>
+#include "roster.h"
 
 using namespace std;
+
+//REFERENCE:
+//"A1,John,Smith,John1989@gmail.com,20,30,35,40,SECURITY"
+//find(item, index) | substr(index, length)
+
+//parse function definition
+void Roster::parse(string student) {
+	//size_t is the unsigned integer data type
+	size_t rhs = student.find(","); //find the comma to set it as the value of right hand side for substr
+	string studentId = student.substr(0, rhs);//get substr starting at index 0 to rhs value from above.
+
+	size_t lhs = rhs + 1; //left hand side set to above rhs plus one index to move past previos comma
+	rhs = student.find(",", lhs); //find comma starting at index lhs
+	string fName = student.substr(lhs, rhs - lhs); //get substr fname at index lhs having rhs-lhs length chars
+	
+	lhs = rhs + 1;
+	rhs = student.find(",", lhs);
+	string lName = student.substr(lhs, rhs - lhs);
+
+	lhs = rhs + 1;
+	rhs = student.find(",", lhs);
+	string email = student.substr(lhs, rhs - lhs);
+
+	lhs = rhs + 1;
+	rhs = student.find(",", lhs);
+	int age = stoi(student.substr(lhs, rhs - lhs)); //string to integer
+
+	lhs = rhs + 1;
+	rhs = student.find(",", lhs);
+	double days1 = stod(student.substr(lhs, rhs - lhs)); //string to double
+	lhs = rhs + 1;
+	rhs = student.find(",", lhs);
+	double days2 = stod(student.substr(lhs, rhs - lhs));
+	lhs = rhs + 1; 
+	rhs = student.find(",", lhs);
+	double days3 = stod(student.substr(lhs, rhs - lhs));
+
+	lhs = rhs + 1;
+	rhs = student.find(",", lhs);
+	string strDegree = student.substr(lhs, rhs - lhs);
+
+	DegreeProgram degreeProgram = DegreeProgram::SECURITY;
+	if (strDegree == "SECURITY") {
+		strDegree = DegreeProgram::SECURITY;
+	}
+	else if (strDegree == "NETWORK") {
+		strDegree = DegreeProgram::NETWORK;
+	}
+	else if (strDegree == "SOFTWARE") {
+		strDegree = DegreeProgram::SOFTWARE;
+	}
+	
+}
--- roster.h
+++ roster.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "student.h"
+
+
+class Roster {
+private:
+	//declare local variable for studentData
+	const static int studentData = 5;
+	//array of pointer
+	Student* classRosterArray[studentData];
+public:
+	//parse method
+	void parse(string student);
+
+};
+
+//FIXME: change to const static variable
+//int Roster::studentData = 5;
+
+//E2. Create a student object for each student in the data table and populate in the "studentData Table"
+//a. parse each set of data identified in the studentData Table
